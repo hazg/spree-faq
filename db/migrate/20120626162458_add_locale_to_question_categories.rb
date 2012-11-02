e46003210def1535120d88336d6d5c63ecf60e2a
hazg <hazg@mail.ru>
2012-11-02 17:01:27 +0400

    Locale fixes + russian translation

--- db/migrate/20120626162458_add_locale_to_question_categories.rb
+++ db/migrate/20120626162458_add_locale_to_question_categories.rb
@@ -1,7 +1,7 @@
 class AddLocaleToQuestionCategories < ActiveRecord::Migration
   def self.up
     add_column :question_categories, :locale, :string
-    QuestionCategory.update_all :locale => Spree::Config[:default_locale] 
+    QuestionCategory.update_all :locale => I18n.default_locale
   end
  
   def self.down
