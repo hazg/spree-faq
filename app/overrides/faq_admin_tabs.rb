436c87269130b184cdbea48b30ef04125054b40c
hazg <hazg@mail.ru>
2012-11-02 17:27:14 +0400

    1.3 icon in admin menu

--- app/overrides/faq_admin_tabs.rb
+++ app/overrides/faq_admin_tabs.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => 'faq_admin_tabs',
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text =>  %[<%=  tab(:question_categories, :label => :question_categories_admin)  %>])
+                     :text =>  %[<%=  tab(:question_categories, :icon => 'icon-question-sign', :label => :question_categories_admin)  %>])
